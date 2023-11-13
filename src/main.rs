c66eeef3089dda0259069a90168b6f272a74f614
RedRam <your@email.com>
2023-11-12 22:59:02 -0800

    Use ring buffer of ticks instead of page flipping GameState. Seperate global state into its own struct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use interp_test::time::Timer;
-use interp_test::{Player, DBG_INTERP, DBG_PREV, DBG_NOW, dbg_arrow};
+use std::collections::VecDeque;
+
 use interp_test::movement::Movement;
+use interp_test::time::Timer;
+use interp_test::{dbg_arrow, Player, DBG_INTERP, DBG_NOW, DBG_PREV};
 use macroquad::prelude::*;
 use macroquad::window::{screen_height, screen_width};
 
@@ -8,7 +10,9 @@ use macroquad::window::{screen_height, screen_width};
 // const UPDATES_PER_SECOND: f32 = 15.0;
 // const TICK_LEN_SECS: f32 = 1.0 / UPDATES_PER_SECOND * 10.0;
 const UPDATES_PER_SECOND: f32 = 30.0;
-const TICK_LEN_SECS: f32 = 1.0 / UPDATES_PER_SECOND * 10.5;
+const TICK_LEN_SECS: f32 = 1.0 / UPDATES_PER_SECOND * 5.0;
+const TICK_BUFFER_SECS: f32 = 0.25;
+const TICK_BUFFER_LEN: usize = (TICK_BUFFER_SECS * UPDATES_PER_SECOND + 0.5) as usize; // + 0.5 as ceil is not const (wtf)
 
 /// TODO:DOCS:
 const SPEED_FACTOR: f32 = 60.0 / UPDATES_PER_SECOND;
@@ -17,27 +21,117 @@ const PLAYER_ACCEL: f32 = 5.0 * SPEED_FACTOR * SPEED_FACTOR;
 
 #[derive(Clone, Debug, PartialEq, Default)]
 struct GameState {
+    // /// Tick index, icreases every tick
+    tick_number: usize,
+    /// older ticks in front, newer in back
+    tick_state: VecDeque<TickState>,
+    // no global_state here because of (re)borrow issues
+    // cant do self.prev_tick_mut() and &self.global_state
+    // global_state: GlobalState,
+}
+
+// #[allow(dead_code)]
+impl GameState {
+    fn new() -> Self {
+        // Self::default()
+        Self {
+            tick_state: VecDeque::with_capacity(TICK_BUFFER_LEN),
+            ..Self::default()
+        }
+    }
+
+    fn init(&mut self) -> &mut Self {
+        // let player = self.tick_state.front_mut().unwrap();
+        dbg!(self.tick_state.len());
+        let mut player = Player::default();
+        player.movement.pos.x = screen_width() / 2.0;
+        player.movement.pos.y = screen_height() / 2.0;
+
+        let first_tick = TickState { player };
+        for _ in 0..self.tick_state.capacity() {
+            self.tick_state.push_back(first_tick.clone());
+            // *state = first_tick.clone()
+        }
+
+        self
+    }
+
+    fn current_tick(&self) -> &TickState {
+        self.tick_state.back().unwrap()
+    }
+    fn current_tick_mut(&mut self) -> &mut TickState {
+        self.tick_state.back_mut().unwrap()
+    }
+
+    fn prev_tick(&self) -> &TickState {
+        self.get_prev_tick(1).unwrap()
+    }
+    fn prev_tick_mut(&mut self) -> &TickState {
+        self.get_prev_tick_mut(1).unwrap()
+    }
+
+    /// Get the tick `tick` ticks in the past. 0 is current tick, 1 is previous tick.
+    fn get_prev_tick(&self, tick: usize) -> Option<&TickState> {
+        // VecDeque::back(): self.get(self.len.wrapping_sub(1))
+        self.tick_state
+            .get(self.tick_state.len().wrapping_sub(1 + tick))
+    }
+    /// Get the tick `tick` ticks in the past. 0 is current tick, 1 is previous tick.
+    fn get_prev_tick_mut(&mut self, tick: usize) -> Option<&mut TickState> {
+        // VecDeque::back_mut(): self.get_mut(self.len.wrapping_sub(1))
+        self.tick_state
+            .get_mut(self.tick_state.len().wrapping_sub(1 + tick))
+    }
+
+    fn get_tick(&self, tick_number: usize) -> Option<&TickState> {
+        let current = self.tick_number;
+        // None if tried get future tick. NOTE:CHEATS: should never happen
+        let prev = current.checked_sub(tick_number)?;
+        self.get_prev_tick(prev) // None if tried to get too old
+    }
+    fn get_tick_mut(&mut self, tick_number: usize) -> Option<&mut TickState> {
+        let current = self.tick_number;
+        // None if tried get future tick. NOTE:CHEATS: should never happen
+        let prev = current.checked_sub(tick_number)?;
+        self.get_prev_tick_mut(prev) // None if tried to get too old
+    }
+
+    /// Remove oldest tick, copy latest tick to current. Returns the now current tick
+    /// (unmodified from the now previous tick)
+    fn advance_tick(&mut self) -> &mut TickState {
+        // NOTE:PANIC: only panics when not `init()`ed
+
+        self.tick_number += 1;
+        let latest_tick = self.tick_state.back_mut().unwrap().clone();
+        self.tick_state.pop_front(); // remove oldest
+        self.tick_state.push_back(latest_tick); // copy latest
+        self.tick_state.back_mut().unwrap()
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Default)]
+struct TickState {
     player: Player,
+}
+
+#[derive(Clone, Debug, PartialEq, Default)]
+struct GlobalState {
     /// Store input as fast as possible here until `update()`
     input_buffer: Vec<Vec2>,
 }
 
-impl GameState {
-    fn new() -> Self {
-        Self::default()
-    }
-    fn init(&mut self) -> &mut Self {
-        self.player.movement.pos.x = screen_width() / 2.0;
-        self.player.movement.pos.y = screen_height() / 2.0;
-        self
-    }
-}
+// #[derive(Clone, Debug, PartialEq, Default)]
+// struct ServerState {
+
+// }
 
 #[macroquad::main("interp test")]
 async fn main() {
     let mut game_state = GameState::new();
     game_state.init();
-    let mut prev_game_state = game_state.clone();
+    dbg!(game_state.tick_state.len());
+    let mut global_state = GlobalState::default();
+    // let mut prev_game_state = game_state.clone();
     // let game = &mut game_state;
     // let mut prev_game = &mut prev_game_state;
     let mut update_timer = Timer::new(TICK_LEN_SECS);
@@ -50,11 +144,12 @@ async fn main() {
         let ready_to_update = update_timer.decrement(delta_time);
 
         // Input and Update
-        handle_inputs(&mut game_state.input_buffer); // as close to update as possible
+        handle_inputs(&mut global_state.input_buffer); // as close to update as possible
         if ready_to_update {
-            prev_game_state = game_state.clone();
-            update(&mut game_state);
-            game_state.input_buffer.clear()
+            // prev_game_state = game_state.clone();
+            game_state.advance_tick();
+            update(&mut game_state, &mut global_state);
+            global_state.input_buffer.clear()
         }
 
         if is_key_pressed(KeyCode::Q) {
@@ -66,23 +161,20 @@ async fn main() {
         if dont_interpolate {
             tick_progress = 1.0;
         }
-        draw(&game_state, &prev_game_state, tick_progress);
+        draw(&game_state, &global_state, tick_progress);
 
         next_frame().await; // forced vsync :/
     }
 }
 
-fn update(game: &mut GameState) {
-    let player = &mut game.player;
+fn update(game: &mut GameState, global_state: &mut GlobalState) {
+    let player = &mut game.current_tick_mut().player;
     let center = Vec2::new(screen_width() / 2.0, screen_height() / 2.0);
     player
-        .handle_movement(&game.input_buffer, PLAYER_ACCEL)
+        .handle_movement(&global_state.input_buffer, PLAYER_ACCEL)
         // .movement
         // .limit_speed(PLAYER_MAX_SPEED)
         ;
-
-    dbg!(player.movement.vel.length());
-    dbg!(player.movement.accel.length());
 
     if is_key_down(KeyCode::Space) {
         player.movement = Movement::default();
@@ -92,19 +184,29 @@ fn update(game: &mut GameState) {
     player.movement.step(PLAYER_MAX_SPEED);
 }
 
-fn draw(game: &GameState, prev: &GameState, t: f32) {
+// global state only needed for debug stuff rn
+fn draw(game: &GameState, global_state: &GlobalState, t: f32) {
+    let current = game.current_tick();
+    let prev = game.prev_tick();
     clear_background(GRAY);
-    game.player.draw(&prev.player, t);
+    current.player.draw(&prev.player, t);
     // game.player.draw(&prev.player, 0.25);
     // game.player.draw(&prev.player, 0.5);
     // game.player.draw(&prev.player, 0.75);
-    game.player.draw_dbg_prev(&prev.player, t);
-    game.player.draw_dbg(&prev.player, t);
-    let real_time_avg_dir = interp_test::average_input(&game.input_buffer);
-    let interpolated_pos = prev.player.movement.interp(&game.player.movement, t);
-    dbg_arrow(interpolated_pos, real_time_avg_dir * 50.0, DBG_INTERP);
-    dbg_arrow(interpolated_pos, game.player.movement.accel, DBG_PREV);
-    dbg_arrow(interpolated_pos, game.player.movement.vel, DBG_NOW);
+    current.player.draw_dbg_prev(&prev.player, t);
+    current.player.draw_dbg(&prev.player, t);
+    let real_time_avg_dir = interp_test::average_input(&global_state.input_buffer);
+    let interped_pos = prev.player.movement.interp(&current.player.movement, t);
+    dbg_arrow(interped_pos, real_time_avg_dir * 50.0, DBG_INTERP);
+    dbg_arrow(interped_pos, current.player.movement.accel, DBG_PREV);
+    dbg_arrow(interped_pos, current.player.movement.vel, DBG_NOW);
+
+    // dbg tick buffer
+    for i in (0..TICK_BUFFER_LEN - 1).rev() {
+        let prev = &game.get_prev_tick(i + 1).unwrap().player;
+        let next = &game.get_prev_tick(i).unwrap().player;
+        next.draw(prev, t);
+    }
 }
 
 fn handle_inputs(input_buffer: &mut Vec<Vec2>) {
--- src/movement.rs
+++ src/movement.rs
@@ -1,7 +1,7 @@
 #![allow(dead_code)] // TODO: just remove them?
 
-use crate::spline;
-use macroquad::color::*;
+// use crate::spline;
+// use macroquad::color::*;
 use macroquad::math::Vec2;
 
 use super::lerp_fast2;
@@ -14,17 +14,17 @@ pub struct Movement {
     pub vel: Vec2,
     /// accel needed to get current vel
     pub accel: Vec2,
-    pub prev_pos: Vec2,
-    pub prev_vel: Vec2,
-    pub prev_accel: Vec2,
+    // pub prev_pos: Vec2,
+    // pub prev_vel: Vec2,
+    // pub prev_accel: Vec2,
 }
 
 impl Movement {
     /// Update player velocity and position
     pub fn step(&mut self, max_speed: f32) -> &mut Self {
-        self.prev_pos = self.pos;
-        self.prev_vel = self.vel;
-        self.prev_accel = self.accel;
+        // self.prev_pos = self.pos;
+        // self.prev_vel = self.vel;
+        // self.prev_accel = self.accel;
         self.step_vel().limit_speed(max_speed).step_pos();
         // self.step_pos().step_vel();
         // if self.
@@ -54,6 +54,7 @@ impl Movement {
     pub fn interp(&self, next: &Self, t: f32) -> Vec2 {
         // self.interp0(next, t) // linear
         self.interp3(next, t) // curvy
+
         // horrible:
         // self.interp3_c(next, t)
         // Self::bezier_3(self, next, t)
@@ -61,72 +62,72 @@ impl Movement {
         // Self::bezier_4b(self, next, t)
     }
 
-    fn bezier_3(&self, next: &Self, t: f32) -> Vec2 {
-        // let c1 = self.pos + self.prev_vel / 3.0;
-        // let c1 = self.pos - self.accel;
+    // fn bezier_3(&self, next: &Self, t: f32) -> Vec2 {
+    //     // let c1 = self.pos + self.prev_vel / 3.0;
+    //     // let c1 = self.pos - self.accel;
 
-        // let c1 = next.pos - self.accel;
+    //     // let c1 = next.pos - self.accel;
 
-        let mirror = next.pos - self.pos;
-        let angle = (self.accel).angle_between(mirror);
-        dbg!(angle);
+    //     let mirror = next.pos - self.pos;
+    //     let angle = (self.accel).angle_between(mirror);
+    //     dbg!(angle);
 
-        let mut c1 = (self.pos + next.pos) / 2.0;
-        crate::dbg_line(self.pos, self.pos + (self.vel.normalize()) * 25.0, MAGENTA);
-        c1 += self.vel.normalize() * self.accel.length() * (angle);
-        //asdjihsdayuhsadgyu1
-        crate::dbg_circle(c1);
-        // self.vel.ro
+    //     let mut c1 = (self.pos + next.pos) / 2.0;
+    //     crate::dbg_line(self.pos, self.pos + (self.vel.normalize()) * 25.0, MAGENTA);
+    //     c1 += self.vel.normalize() * self.accel.length() * (angle);
+    //     //asdjihsdayuhsadgyu1
+    //     crate::dbg_circle(c1);
+    //     // self.vel.ro
 
-        spline::bezier_3(self.pos, c1, next.pos, t)
-    }
+    //     spline::bezier_3(self.pos, c1, next.pos, t)
+    // }
 
-    fn bezier_4(&self, next: &Self, t: f32) -> Vec2 {
-        let mirror = next.pos - self.pos;
-        // let mirror = self.vel;
-        // let c1 = self.pos + self.prev_vel / 3.0;
-        // let c1 = self.pos - self.accel;
-        let c1 = self.pos + self.prev_accel;
-        // dbg_circle(c1);
-        // dbg_circle(c1);
-        // dbg_circle(c1);
-        // let c1 = vec2_mirror(c1, mirror, self.pos) + self.pos;
-        crate::dbg_arrow(self.pos, mirror, MAGENTA);
-        // crate::dbg_arrow(self.pos, self.vel * 10.0, BROWN);
-        let angle = (c1 - self.pos).angle_between(mirror);
-        crate::dbg_line(self.pos, c1, RED);
-        dbg!(angle);
-        let c1 = crate::rotate_around_point(c1, self.pos, angle * 2.0);
-        crate::dbg_line(self.pos, c1, GREEN);
-        // let c1 = crate::rotate_around_point(c1, self.pos, angle);
-        // let c1 = (c1 - self.pos).perp().perp().perp() + self.pos;
-        // dbg_circle(c1);
-        // let c1 = (c1 - self.pos).rotate(Vec2::new(0.0, -1.0)) + self.pos;
+    // fn bezier_4(&self, next: &Self, t: f32) -> Vec2 {
+    //     let mirror = next.pos - self.pos;
+    //     // let mirror = self.vel;
+    //     // let c1 = self.pos + self.prev_vel / 3.0;
+    //     // let c1 = self.pos - self.accel;
+    //     let c1 = self.pos + self.prev_accel;
+    //     // dbg_circle(c1);
+    //     // dbg_circle(c1);
+    //     // dbg_circle(c1);
+    //     // let c1 = vec2_mirror(c1, mirror, self.pos) + self.pos;
+    //     crate::dbg_arrow(self.pos, mirror, MAGENTA);
+    //     // crate::dbg_arrow(self.pos, self.vel * 10.0, BROWN);
+    //     let angle = (c1 - self.pos).angle_between(mirror);
+    //     crate::dbg_line(self.pos, c1, RED);
+    //     dbg!(angle);
+    //     let c1 = crate::rotate_around_point(c1, self.pos, angle * 2.0);
+    //     crate::dbg_line(self.pos, c1, GREEN);
+    //     // let c1 = crate::rotate_around_point(c1, self.pos, angle);
+    //     // let c1 = (c1 - self.pos).perp().perp().perp() + self.pos;
+    //     // dbg_circle(c1);
+    //     // let c1 = (c1 - self.pos).rotate(Vec2::new(0.0, -1.0)) + self.pos;
 
-        let c2 = next.pos - self.accel;
-        crate::dbg_circle(c1);
-        crate::dbg_circle(c2);
+    //     let c2 = next.pos - self.accel;
+    //     crate::dbg_circle(c1);
+    //     crate::dbg_circle(c2);
 
-        // dbg_circle(c2);
+    //     // dbg_circle(c2);
 
-        // self.vel.ro
-        spline::bezier_4(self.pos, c1, c2, next.pos, t)
-    }
+    //     // self.vel.ro
+    //     spline::bezier_4(self.pos, c1, c2, next.pos, t)
+    // }
 
-    fn bezier_4b(&self, next: &Self, t: f32) -> Vec2 {
-        let limit_speed = |vec2: Vec2| {
-            let dir = vec2.length().min(40.0) * vec2.normalize_or_zero();
-            dbg!(dir.length());
-            dir
-        };
-        let c1 = self.pos + self.vel / 3.0;
-        // let c2 = next.pos - (next.vel + next.accel) / 3.0;
-        let c2 = next.pos - limit_speed(next.vel + next.accel) / 3.0;
-        crate::dbg_circle(c1);
-        crate::dbg_circle(c2);
-        spline::bezier_4(self.pos, c1, c2, next.pos, t)
-        // spline::bezier_3(self.prev_pos, c1, self.pos, t)
-    }
+    // fn bezier_4b(&self, next: &Self, t: f32) -> Vec2 {
+    //     let limit_speed = |vec2: Vec2| {
+    //         let dir = vec2.length().min(40.0) * vec2.normalize_or_zero();
+    //         dbg!(dir.length());
+    //         dir
+    //     };
+    //     let c1 = self.pos + self.vel / 3.0;
+    //     // let c2 = next.pos - (next.vel + next.accel) / 3.0;
+    //     let c2 = next.pos - limit_speed(next.vel + next.accel) / 3.0;
+    //     crate::dbg_circle(c1);
+    //     crate::dbg_circle(c2);
+    //     spline::bezier_4(self.pos, c1, c2, next.pos, t)
+    //     // spline::bezier_3(self.prev_pos, c1, self.pos, t)
+    // }
 
     /// interp pos only
     pub fn interp0(&self, next: &Self, t: f32) -> Vec2 {
@@ -149,13 +150,13 @@ impl Movement {
 
     /// I dont know how this works.
     /// I've tried for like 3 days of studying kinematics, calculas and splines
-    /// and fiddling around, this is the best 
+    /// and fiddling around, this is the best
     pub fn interp3(&self, next: &Self, t: f32) -> Vec2 {
         // let lerp_dumb = |a, b, t| if t <= 0.5 {a} else {b};
         let t_2 = t * t;
         let nt_2 = 1.0 - t_2;
         let accel = next.vel - self.vel; // because Self.accel lies cuz speed gets capped
-        // dbg!(accel, next.accel);
+                                         // dbg!(accel, next.accel);
         let start_accel = 0.5 * accel * t_2;
         let end_accel = -0.5 * accel * nt_2;
         // let accel = lerp_dumb(start_accel, end_accel, t);
@@ -167,33 +168,33 @@ impl Movement {
         // self.interp2(next, t)
     }
 
-    fn interp3_c(&self, next: &Self, t: f32) -> Vec2 {
-        // let lerp_dumb = |a, b, t| if t <= 0.5 {a} else {b};
-        let t_2 = t * t;
-        let nt = 1.0 - t;
-        let nt_2 = 1.0 - t_2;
+    // fn interp3_c(&self, next: &Self, t: f32) -> Vec2 {
+    //     // let lerp_dumb = |a, b, t| if t <= 0.5 {a} else {b};
+    //     let t_2 = t * t;
+    //     // let nt = 1.0 - t;
+    //     // let nt_2 = 1.0 - t_2;
 
-        // if self.pos != next.prev_pos {
-        //     // println!("bruh");
-        //     panic!()
-        // }
-        // if self.vel != next.prev_vel {
-        //     // println!("bruh");
-        //     dbg!(self.vel);
-        //     dbg!(next.prev_vel);
-        //     panic!()
-        // }
-        // let vel = next.prev_vel * t;
+    //     // if self.pos != next.prev_pos {
+    //     //     // println!("bruh");
+    //     //     panic!()
+    //     // }
+    //     // if self.vel != next.prev_vel {
+    //     //     // println!("bruh");
+    //     //     dbg!(self.vel);
+    //     //     dbg!(next.prev_vel);
+    //     //     panic!()
+    //     // }
+    //     // let vel = next.prev_vel * t;
 
-        let accel = next.vel - self.vel;
-        let vel = next.prev_vel * t;
-        // let vel = self.vel * t; // def no
-        // let vel = next.vel * t;
-        let accel = 1.0 * accel * t_2;
-        let calc_pos = self.pos + vel + accel;
-        lerp_fast2(calc_pos, next.pos, t)
-        // calc_pos
-    }
+    //     let accel = next.vel - self.vel;
+    //     let vel = next.prev_vel * t;
+    //     // let vel = self.vel * t; // def no
+    //     // let vel = next.vel * t;
+    //     let accel = 1.0 * accel * t_2;
+    //     let calc_pos = self.pos + vel + accel;
+    //     lerp_fast2(calc_pos, next.pos, t)
+    //     // calc_pos
+    // }
 
     // pub fn interp4(&self, next: &Self, t: f32) -> Vec2 {
     //     // let lerp_dumb = |a, b, t| if t <= 0.5 {a} else {b};
